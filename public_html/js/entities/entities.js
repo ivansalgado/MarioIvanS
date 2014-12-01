48ece86757db05a4be3049c8a5b9fcc5ff280c73
Ivan Salgado <ivansalgado2000@gmail.com>
2014-12-01 10:39:08 -0800

    created different layers.

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -8,7 +8,7 @@ game.PlayerEntity = me.Entity.extend({
                 width: 128,
                 height: 128,
                 getShape: function(){
-                    return (new me.Rect(0, 0, 128, 128)).toPolygon();
+                    return (new me.Rect(0, 0, 20, 128)).toPolygon();
              }
         }]);
     
--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -8,6 +8,7 @@ game.resources = [
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "mario", type:"image", src: "data/img/player1.png"},
+        {name: "title-screen", type:"image", src: "data/img/title-screen.png"},
 
 	/* Atlases 
 	 * @example
